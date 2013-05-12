0bb1b33d81bee34eabf0ff5912b8127c4a81ae05
Boris <bonbonev@gmail.com>
2013-05-12 18:06:45 +0300

    Subo`s code

--- TestKingSurvivalGame/TestEngine.cs
+++ TestKingSurvivalGame/TestEngine.cs
@@ -12,5 +12,33 @@ namespace TestKingSurvivalGame
         {
             Engine currentEngine = new Engine(null,null);
         }
+
+        // from Subo
+        // [TestMethod]
+        //public void TestScenarioFigurite() // tuka testvame carq da pe4eli
+        //{
+        //    string moves = string.Format("kul bdr kul bdl kur adr kur bdl kur bdl kul ddl kul", Environment.NewLine);
+
+        //    using (StringWriter stringWriter = new StringWriter())
+        //    {
+        //        Console.SetOut(stringWriter);
+
+        //        using (StringReader stringReader = new StringReader(moves))
+        //        {
+        //            Console.SetIn(stringReader);
+
+        //            Engine engine = new Engine(); //tuka s figurite
+
+        //            engine.Run();
+
+        //            string[] outputLines = stringWriter.ToString().Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+        //            string expected = "King wins in 7 turns!";
+        //            string actual = outputLines[outputLines.Length - 1];
+
+        //            Assert.AreEqual(expected, actual);
+        //        }
+        //    }
+        //}
     }
 }
